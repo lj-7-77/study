3a5c9f6e61e6e8227c149a9b519a592c2757ee6d
lj-7-77 <swtw72@gmail.com>
2022-11-05 14:14:29 +0900

    feat: Charger 구조체 추가

--- study1105_pop/main.swift
+++ study1105_pop/main.swift
@@ -4,8 +4,16 @@
 //
 //  Created by LJ on 2022/11/05.
 //
-let charger: Charger = Charger()
+let appleWatchCharger: Charger = Charger(maximumWattPerHour: 5)
+let iphoneRapidCharger: Charger = Charger(maximumWattPerHour: 18)
+let iPadCharger: Charger = Charger(maximumWattPerHour: 30)
+let macBookCharger: Charger = Charger(maximumWattPerHour: 96)
+let macBookProCharger: Charger = Charger(maximumWattPerHour: 106)
 
 struct Charger: Chargeable {
+    let maximumWattPerHour: WattPerHour
     
+    init(maximumWattPerHour: WattPerHour) {
+        self.maximumWattPerHour = maximumWattPerHour
+    }
 }
